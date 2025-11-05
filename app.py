100c1a137454f3e2d67fe82519dad91c4691c8ca
Taran-Saini13 <tarnsaini9713@gmail.com>
2025-11-05 11:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,5 +125,8 @@ def recommend(movie_name: str, top_n=6):
 
 
 # ---------- MAIN ----------
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env var
+    app.run(host="0.0.0.0", port=port)
+
